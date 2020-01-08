5880de1295bd5d71815f07d2de8a1f7be4d6647e
안재철 <qnzo8888@gmail.com>
2020-01-08 09:19:07 +0900

    Create bloodchain.js

---
+++ codeForGuide/bloodchain.js
@@ -0,0 +1,184 @@
+'use strict';
+
+const { Contract } = require('fabric-contract-api');
+
+class BloodChain extends Contract {
+
+    async initLedger(ctx) {
+        console.info('============= START : Initialize Ledger ===========');
+
+        const bloodCards = [
+            {
+                owner: '1',
+                reg_date: new Date().toLocaleDateString(),
+                is_donated: false,
+                donater: null,
+                dona_date: null,
+                is_used: false,
+                used_place: null,
+                used_date: null,
+                docType: 'bloodCard'
+            }
+        ];
+
+        await ctx.stub.putState('test', Buffer.from(JSON.stringify(bloodCards[0])));
+        console.info('Added <--> ', bloodCards[0]);
+
+        console.info('============= END : Initialize Ledger ===========');
+    }
+
+    async getResults(iterator){
+        const allResults = [];
+        while (true) {
+            const res = await iterator.next();
+            if (res.value && res.value.value.toString()) {
+                console.log(res.value.value.toString('utf8'));
+
+                const Key = res.value.key;
+                let Record;
+                try {
+                    Record = JSON.parse(res.value.value.toString('utf8'));
+                } catch (err) {
+                    console.log(err);
+                    Record = res.value.value.toString('utf8');
+                }
+                allResults.push({ Key, Record });
+            }
+            if (res.done) {
+                console.log('end of data');
+                await iterator.close();
+                console.info(allResults);
+                return JSON.stringify(allResults);
+            }
+        }
+    }
+
+    // 모든 헌혈증의 모든 value 검색(확인용, 구현 완료) return : 헌혈증 key, value(record) 문자열화 한 배열
+    async queryAllBloodCards(ctx) {
+        const iterator = await ctx.stub.getQueryResult(`{
+            "selector": {
+                "docType": "bloodCard"
+            }
+        }`);
+        return this.getResults(iterator);
+    }
+
+    //등록 o 기부 x    return : reg_date
+    async queryBloodCardsOnlyReg(ctx, owner) {
+        const iterator = await ctx.stub.getQueryResult(`{
+            "selector": {
+                "docType": "bloodCard",
+                "is_donated": false,
+                "owner": "${owner}"
+            },
+            "fields": ["reg_date"]
+        }`);
+        return this.getResults(iterator);
+    }
+
+    // 기부 o 사용 o | x  return 
+    async queryBloodCardsDona(ctx, donater) {
+        const iterator = await ctx.stub.getQueryResult(`{
+            "selector": {
+                "docType": "bloodCard",
+                "donater": "${donater}"
+            }
+        }`);
+        return this.getResults(iterator);
+    }
+
+    // 기부받은 헌혈증 확인
+    async queryBloodCardsDonated(ctx, owner) {
+        const iterator = await ctx.stub.getQueryResult(`{
+            "selector": {
+                "docType": "bloodCard",
+                "is_donated": true,
+                "owner": "${owner}"
+            }
+        }`);
+        return this.getResults(iterator);
+    }
+
+    // 헌혈증 등록 return : x
+    async register(ctx, serialNumber, owner) {
+        console.info('============= START : Create bloodCard ===========');
+        const bloodCard = {
+            owner,
+            reg_date: new Date().toLocaleDateString(),
+            is_donated: false,
+            donater: null,
+            dona_date: null,
+            is_used: false,
+            used_place: null,
+            used_date: null,
+            docType: 'bloodCard',
+        };
+
+        await ctx.stub.putState(serialNumber, Buffer.from(JSON.stringify(bloodCard)));
+        console.info('============= END : Create bloodCard ===========');
+    }
+
+    // 기부할 헌혈증들의 serial 번호를 쿼리 return : serials[]
+    async querySerialsForDonate(ctx, donate_count, donater) {
+        console.info('============= START : donate ===========');
+
+        const { iterator, metadata }= await ctx.stub.getQueryResultWithPagination(`{
+            "selector": {
+                "reg_date": {"$ne": null},
+                "docType": "bloodCard", 
+                "owner": "${donater}", 
+                "is_donated": false
+            },
+            "sort": [
+                {"reg_date": "asc"}
+            ]
+        }`, Number(donate_count));
+        console.log(metadata);
+
+        var serials = [];
+
+        while (true) {
+            const res = await iterator.next();
+            if (res.value && res.value.value.toString()) {
+                console.log(res.value.value.toString('utf8'));
+
+                const serial = res.value.key;
+                
+                serials.push(serial);
+            }
+            if (res.done) {
+                await iterator.close();
+                console.log(serials);
+                console.info('============= END : donate ===========');
+                return JSON.stringify(serials);
+            }
+        }
+    }
+
+    // getDonatedSerial 로 가져온 serial들 중 serial 하나에 해당하는 헌혈증을 기부처리
+    async donate(ctx, serial, newOwner, used_place){
+        var value = await ctx.stub.getState(serial);
+        var bloodCard = JSON.parse(value.toString('utf8'));
+        bloodCard.donater = bloodCard.owner;
+        bloodCard.owner = newOwner;
+        bloodCard.is_donated = true;
+        bloodCard.dona_date = new Date().toLocaleDateString();
+        bloodCard.used_place = used_place;
+        await ctx.stub.putState(serial, Buffer.from(JSON.stringify(bloodCard)));
+    }
+
+    // 헌혈증 사용 return : x
+    async useCard(ctx, serial){
+        var value = await ctx.stub.getState(serial);
+        if (!value || value.length === 0) {
+            throw new Error(`${serial} does not exist`);
+        }
+        var bloodCard = JSON.parse(value.toString('utf8'));
+        bloodCard.is_used = true;
+        bloodCard.used_date = new Date().toLocaleDateString();
+        await ctx.stub.putState(serial, Buffer.from(JSON.stringify(bloodCard)))
+    }
+
+}
+
+module.exports = BloodChain;
